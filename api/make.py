8f346c017c4ea9650a79f2918ec8373e9e1ca038
davidleonstr <davidalfonsoleoncarmona@gmail.com>
2026-06-14 00:14:07 -0600

    Open CORS

--- api/make.py
+++ api/make.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from api.config import Config, DB
 from api.v1.models import *
+from flask_cors import CORS
 
 from api.v1 import v1BP
 
 def make() -> Flask:
     app = Flask(__name__)
+
+    CORS(app)
 
     app.register_blueprint(v1BP, url_prefix='/v1')
     app.config.from_object(Config)
